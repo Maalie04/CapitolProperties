05d051ee7aa3678b489206de623058d8f726913a
Jamaal Lawson <maaliemaal@gmail.com>
2021-07-05 12:36:13 -0400

    updating our js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ var usefrHandler = function (event) {
 
 
 
-url: 'https://real-estate-usa.p.rapidapi.com/api/v1/properties'
-params: {postal_code: '94105', offset: '0', limit: '200'},
-headers: {
-  'x-rapidapi-key': 'b2f0d79ea0mshef6e2494270b43ap1807fejsn68dd68d4adae',
-  'x-rapidapi-host': 'real-estate-usa.p.rapidapi.com';
+// url: 'https://real-estate-usa.p.rapidapi.com/api/v1/properties'
+// params: {postal_code: '94105', offset: '0', limit: '200'},
+// headers: {
+//   'x-rapidapi-key': 'b2f0d79ea0mshef6e2494270b43ap1807fejsn68dd68d4adae',
+//   'x-rapidapi-host': 'real-estate-usa.p.rapidapi.com';
