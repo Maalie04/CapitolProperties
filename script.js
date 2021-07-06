201bb887d638c0246df108f654fb4c9bc4b016d7
Mike Sowers <msowers72@yahoo.com>
2021-07-06 18:50:29 -0400

    added html and js changes

---
+++ script.js
@@ -0,0 +1,15 @@
+var searchBtn = document.querySelector("#search-btn");
+
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault(event);
+    console.log("Im working");
+})
+
+
+
+
+
+
+
+
