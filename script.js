9034bf5f057260bc778267522c6f5f2e4f27e690
Jamaal Lawson <maaliemaal@gmail.com>
2021-07-10 10:54:36 -0400

    adding js file

--- script.js
+++ script.js
@@ -34,9 +34,6 @@ fetch("https://realty-in-us.p.rapidapi.com/properties/v2/list-for-sale?city=Anna
     .catch(err => {
         console.error(err);
     });
-
-
-
 var searchbtn = document.getElementById('search-btn')
 var price = document.getElementById('price');
 var contacts = document.getElementById('contacts');
@@ -67,62 +64,17 @@ function getApi() {
             console.log(response.listings[0].price)
             console.log(response.listings[0].address)
             for (let i = 0; i < 6; i++) {
-                // var listingPrice = response.listings[i].price
-                // var listingPhoto = response.listings[i].photo
-                // var listingAddress = response.listings[i].address
-                // var listingBaths = response.listings[i].bath
-                // var listingBeds = response.listings[i].beds
-                // var listingSqft = response.listings[i].sqft
-
                 // card 1
-               
                 $(".card-text" + i).text("Price: " + response.listings[i].price);
-               
                 $(".bed-text" + i).text("Bed-Rooms: " + response.listings[i].beds);
-               
                 $(".square-text" + i).text("Square-Feet: " + response.listings[i].sqft);
                 $(".img" + i).attr("src",response.listings[i].photo);
-               
-
                 console.log(i);
                 console.log("card.text" + i);
-
-searchbtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('im amazing');
-})
-
-function getApi() {
-    fetch("https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=MD&city=Annapolis&offset=0&limit=200&sort=relevance", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "80d8f34458msh69ffe15bf82e868p178d66jsnac41a9fd8412",
-            "x-rapidapi-host": "realty-in-us.p.rapidapi.com"
-        }
-    })
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            console.log(response.listings[0].price)
-            console.log(response.listings[0].address)
-            for (let i = 0; i < 3; i++) {
-                var listingPrice = response.listings[i].price
-                console.log(response.listings[i].price);
-                var listingPhoto = response.listings[i].photo
-                var listingAddress = response.listings[i].address
-                var listingBaths = response.listings[i].bath
-                var listingBeds = response.listings[i].beds
-                var listingSqft = response.listings[i].sqft
-                $(".card-text" + i).text("");
-               
-                $(".card-text" + i).append("Price" + response.listings[i].price);
-                // $(".card-text1").append("Price" + response.listings[i].price);
-                // $(".card-text2").append("Price" +response.listings[i].price);
             }
         })
         .catch(err => {
             console.error(err);
         });
 }
-getApi();
-//gotta create a function to call into another function to run the Api Function.
+getApi()
