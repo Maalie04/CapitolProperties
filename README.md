eaca7458592cb9b99e20fc42cd5b458098b20f0f
Mike Sowers <msowers72@yahoo.com>
2021-07-12 19:44:06 -0400

    updates

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# CapitolProperties
+## Capitol Properties LLC
+
+## Welcome to our GitHub Repository
+Purchasing, a home is considered a key part of the American dream, however, finding the right home can be an extremely daunting task. Capitol Properties LLC has created a simple and user friendly application to aid you in your home buying experience.
+
+## Scope of Project
+Current Version
+"*" Home page includes the ability to serach home properties for sale in any city in the United States
+"*" Home page includes a search bar that allows user to search city and state
+"*" Upto six properties are displayed
+"*" A current picture of the property is displayed
+"*" Howver feature enlarges the picture of the property
+"*" The properties sales price displays
+"*" The number of bedrooms displays
+"*" The properties square footage is displayed
+
+## Future Ideas
+"*" Information about Real Estate Agent including broker information
+"*" Potential tools include a mortgage calculator, information on local schools, neighborhoods, crime statistics
+"*" Ability to filter listing search results based on search criteria
+"*" Client testimonials
+
+## Tehnology Used
+"*" Hypertext Markup Language (HTML)
+"*" Cascading Style Sheets (CSS)
+"*" JavaScript
+"*" JQuery
+"*" Materializecss.com
+
+## Project Contributors
+"*" Geb Berhea gerhea@gail.com
+"*" Sheldon Fletcher Shelcodingdon@gmail.com
+"*" Karim Gomez karim.gomez29@yahoo.com
+"*" Jamaal Lawson maaliemaal@yahoo.com
+"*" Mike Sowers msowers72@yahoo.com
+
+## Images & Wireframes
--- script.js
+++ script.js
@@ -54,41 +54,88 @@ searchbtn.addEventListener('click', function (event) {
     console.log('im amazing');
 })
 function getApi() {
-    fetch("https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=MD&city=Annapolis&offset=0&limit=200&sort=relevance", {
+    fetch("https://realty-in-us.p.rapidapi.com/properties/auto-complet?input=dallas", {
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "80d8f34458msh69ffe15bf82e868p178d66jsnac41a9fd8412",
-            "x-rapidapi-host": "realty-in-us.p.rapidapi.com"
+
+
+        //     fetch("https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=MD&city=Annapolis&offset=0&limit=200&sort=relevance", {
+        // "method": "GET",
+        // "headers": {
+
+            'x-rapidapi-key': 'f9f8fdbbf6msh903f7a4208de85cp1705ebjsn44f85ee69786',
+            'x-rapidapi-host': 'realty-in-us.p.rapidapi.com'
         }
     })
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            console.log(response.listings[0].price)
-            console.log(response.listings[0].address)
+            // console.log(response.listings[0].price)
+            // console.log(response.listings[0].address)
             for (let i = 0; i < 6; i++) {
-                // var listingPrice = response.listings[i].price
+
+                // card 1
+
+                $(".card-text" + i).text("Price: " + response.listings[i].price);
+
+                $(".bed-text" + i).text("Bed-Rooms: " + response.listings[i].beds);
+
+                $(".square-text" + i).text("Square-Feet: " + response.listings[i].sqft);
+                $(".img" + i).attr("src", response.listings[i].photo);
+
+
+                console.log(i);
+                console.log("card.text" + i);
+
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+getApi()
+
+
+// test function we were trying
+var postalCodes = ["21401", "21402", "21403", "21404", "21411"]
+
+var cityArray = JSON.parse(localStorage.getItem('cities')) || [];
+
+var subbmitHandler = function (event) {
+    event.preventDefault();
+
+    var cityName = $('#inputValue').val().trim();
+
+    if (cityName === '') {
+        return;
+    }
+
+    getApi(cityName);
+    localStorage.setItem('cities', JSON.stringify(cityArray));
+}
+
+
+
+
+
+
+
+
+
+
+
+// var listingPrice = response.listings[i].price
                 // var listingPhoto = response.listings[i].photo
                 // var listingAddress = response.listings[i].address
                 // var listingBaths = response.listings[i].bath
                 // var listingBeds = response.listings[i].beds
                 // var listingSqft = response.listings[i].sqft
 
-                // card 1
-               
-                $(".card-text" + i).text("Price: " + response.listings[i].price);
-               
-                $(".bed-text" + i).text("Bed-Rooms: " + response.listings[i].beds);
-               
-                $(".square-text" + i).text("Square-Feet: " + response.listings[i].sqft);
-                $(".img" + i).attr("src",response.listings[i].photo);
-               
-
-                console.log(i);
-                console.log("card.text" + i);
 
 
-                // card 2
+
+
+// card 2
                 // $(".card-text1").text("");
                 // $(".card-text1").append("Price: " + response.listings[i].price);
                 // $(".bed-text1").text("");
@@ -103,12 +150,3 @@ function getApi() {
                 // $(".bed-text2").append("Bed-Rooms: " + response.listings[i].beds);
                 // $(".square-text2").text("");
                 // $(".square-text2").append("Square-Feet: " + response.listings[i].sqft);
-
-
-            }
-        })
-        .catch(err => {
-            console.error(err);
-        });
-}
-getApi()
